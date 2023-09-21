49a892a2d47986d9a9075e822e74797346d79371
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-09-21 14:50:26 -0300

    Update 03_preProc_table.R

--- 03_preProc_table.R
+++ 03_preProc_table.R
@@ -9,23 +9,56 @@ options(scipen= 9e3)
 
 ######################## join protected areas table ##############################
 ## read table
-protected_areas <- read.csv('./tab/areas-to-no-mapa-protected-areas.csv')
+protected_areas <- read.csv('./tab/col_8/protected-areas-lulcc.csv')
 
 ## insert territory type
 protected_areas$territory <- 'Protected Area'
 
 ## read protected areas shapefile
-vec <- as.data.frame(read_sf('./vec/protected_areas.shp'))
+vec <- as.data.frame(read_sf('./vec/new/toAssses/protected_areas_Assess.shp'))
 
 ## get only interest colums
-vec <- vec %>% select('OBJECTID', 'nome_uc', 'esfera', 'grupo', 'categoria', 'NM_MESO', 'SIGLA_UF')
+vec <- vec %>% select('Ano_fim', 'NOME', 'Categoria', 'ID', 'NM_MESO')
 
 ## join tables
-protected_areas <- left_join(x= protected_areas, y= vec, by= c('objectid' = 'OBJECTID'))
+protected_areas <- na.omit(left_join(x= protected_areas, y= vec, by= c('objectid' = 'ID')))
+
+## remove undesirable columns (gee residuals)
+protected_areas <-  protected_areas[ , -which(names(protected_areas) %in% c("system.index",".geo"))]
+
+## translate condition (within or buffer)
+protected_areas$condition <- gsub(1, 'Within', 
+                                gsub(2, 'Buffer zone', protected_areas$condition))
+
+## translate mapbiomas classes
+mapbiomas_dict <- read.csv('./dict/mapbiomas-dict-en-col8.csv', sep= ';')
+
+## translate lulc class
+## create recipe to translate mapbiomas classes
+recipe <- as.data.frame(NULL)
+## for each tenure id
+for (i in 1:length(unique(protected_areas$class_id))) {
+  ## for each unique value, get mean in n levels
+  y <- subset(mapbiomas_dict, id == unique(protected_areas$class_id)[i])
+  ## select matched class
+  z <- subset(protected_areas, class_id == unique(protected_areas$class_id)[i])
+  ## apply translation 
+  z$class_level_0 <- gsub(paste0('^',y$id,'$'), y$mapb_0, z$class_id)
+  z$class_level_1 <- gsub(paste0('^',y$id,'$'), y$mapb_1, z$class_id)
+  z$class_level_1n <- gsub(paste0('^',y$id,'$'), y$mapb_1_2, z$class_id)
+  z$class_level_2 <- gsub(paste0('^',y$id,'$'), y$mapb_2, z$class_id)
+  z$class_level_3 <- gsub(paste0('^',y$id,'$'), y$mapb_3, z$class_id)
+  z$class_level_4 <- gsub(paste0('^',y$id,'$'), y$mapb_4, z$class_id)
+  ## bind into recipe
+  recipe <- rbind(recipe, z)
+}; rm(y, z)
+
+## export table
+write.csv(recipe, './toRead/protected-areas.csv')
+
 
 ##################### join communities table ##################################
-
-communities <- read.csv('./tab/areas-to-no-mapa-communities.csv')
+communities <- read.csv('./tab/col_8/communities-lulcc.csv')
 
 ## insert territory type
 communities$territory <- 'Community'
@@ -42,36 +75,22 @@ communities <- left_join(x= communities, y= vec, by= c('objectid' = 'id_12'))
 ## empty temp files
 rm(vec)
 
-############# compatibilize tables ################
-names(communities)[11] <- 'name'
-names(protected_areas)[9] <- 'name'
-
-## for each unique column in protected areas, insert the same with null values in communities
-toSet <- names(protected_areas[which(names(protected_areas) %in% names(communities) == FALSE)])
-
-for (i in 1:length(toSet)) {
-  communities[as.character(toSet[i])] <- NA
-}
-
-## bind data
-data <- rbind(communities, protected_areas)
-rm(communities, protected_areas)
-
 ## remove undesirable columns (gee residuals)
-data <-  data[ , -which(names(data) %in% c("system.index",".geo"))]
+communities <-  communities[ , -which(names(communities) %in% c("system.index",".geo"))]
 
-## import mapbiomas dictionary
-mapbiomas_dict <- read.csv('./dict/mapbiomas-dict-ptbr.csv', sep= ';')
+## translate condition (within or buffer)
+communities$condition <- gsub(1, 'Within', 
+                                gsub(2, 'Buffer zone', communities$condition))
 
 ## translate lulc class
 ## create recipe to translate mapbiomas classes
 recipe <- as.data.frame(NULL)
 ## for each tenure id
-for (l in 1:length(unique(data$class_id))) {
+for (i in 1:length(unique(communities$class_id))) {
   ## for each unique value, get mean in n levels
-  y <- subset(mapbiomas_dict, id == unique(data$class_id)[l])
+  y <- subset(mapbiomas_dict, id == unique(communities$class_id)[i])
   ## select matched class
-  z <- subset(data, class_id == unique(data$class_id)[l])
+  z <- subset(communities, class_id == unique(communities$class_id)[i])
   ## apply translation 
   z$class_level_0 <- gsub(paste0('^',y$id,'$'), y$mapb_0, z$class_id)
   z$class_level_1 <- gsub(paste0('^',y$id,'$'), y$mapb_1, z$class_id)
@@ -81,12 +100,11 @@ for (l in 1:length(unique(data$class_id))) {
   z$class_level_4 <- gsub(paste0('^',y$id,'$'), y$mapb_4, z$class_id)
   ## bind into recipe
   recipe <- rbind(recipe, z)
-}; rm(data, y, z, mapbiomas_dict)
+}; rm(y, z)
+
+## export table
+write.csv(recipe, './toRead/communities.csv')
 
 
-## translate condition (within or buffer)
-recipe$condition <- gsub(1, 'Within', 
-                       gsub(2, 'Buffer zone', recipe$condition))
 
-## exportar
-write.csv(recipe, './to-no-mapa-data.csv')
+
